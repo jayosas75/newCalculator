71a86891ef9048bfdcbdb3cb531632a176b4831f
John Yosas <jayosas75@gmail.com>
2017-01-31 15:13:58 -0800

    fixer-upper - John Yosas

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 var my_calculator = new Calculator();
 
-/*function callback(type, value, item) {
-    switch (value) {
-        case undefined:
-            $('#display_area').text("");
-    break;
-default:
-    $('#display_area').text(value);
-    break;
-}
-}*/
-
 $(document).ready(function() {
     console.log('hello');
     $('button').click(my_calculator.button_clicked);
@@ -18,56 +7,79 @@ $(document).ready(function() {
 
     function Calculator() {
         var self = this;
-        var inputs_array = [''];
-        var index = 0;
+        this.array_input = [];
+        this.index = 0;
+        this.last_number = null;
+        this.last_operator = null;
+        this.last_item = {};
 
         this.button_clicked = function () {
-
             var clicked_value = $(this).val();
             var clicked_class = $(this).attr('class');
 
-            if (clicked_class === 'btn btn-default number_button') {
+            if (clicked_class === 'number_button') {
                 console.log('number clicked');
                 self.add_number(clicked_value);
-            } else if (clicked_class === 'btn btn-default operator_button') {
+            } else if (clicked_class === 'operator_button') {
                 console.log('operator clicked');
                 self.add_operator(clicked_value);
-            } else if (clicked_class === 'btn btn-default equate_button') {
+            } else if (clicked_class === 'equate_button') {
                 console.log('equal button clicked');
                 self.equate();
-            } else if (clicked_class === 'clear_all btn btn-default buttons') {
+            } else if (clicked_class === 'clear_all') {
                 console.log('clear all button clicked');
                 self.delete_all();
-            } else if (clicked_class === 'clear_one btn btn-default buttons') {
+            } else if (clicked_class === 'clear_one') {
                 console.log('clear one button clicked');
                 self.delete_one();
             }
         };
 
-
+        this.add_number = function(clicked_value){
+            var last_input= null;
+            if(self.array_input.length && self.array_input[self.index - 1].type === 'number') {
+                self.index--;
+                last_input = self.array_input[self.index].value;
+                if(!self.array_input[self.index].total){
+                    if(clicked_value === '.' && last_input.indexOf('.') > -1){
+                        clicked_value = last_input;
+                    }
+                } else if(last_input === '0'){
+                    clicked_value = clicked_value;
+                } else if(clicked_value === '-' || clicked_value === '*'){
+                    if(last_input.indexOf('-') > -1){
+                        clicked_value = last_input.replace('-', '');
+                    } else if(last_input.indexOf('*') > -1){
+                        clicked_value = last_input.replace('*', '');
+                    } else if(last_input.length < 11) {
+                        clicked_value = '-' + last_input;
+                    } else {
+                        clicked_value = last_input;
+                    }
+                } else {
+                    clicked_value += last_input;
+                }
+            } else{
+                self.last_number = null;
+                self.last_operator = null;
+                self.last_item = {};
+            }
+            if(clicked_value === '.'){
+                clicked_value = '0.';
+            }
+            clicked_value = clicked_value.slice(0, 11);
+            self.last_item = {"type": 'number', "value": clicked_value};
+            self.index++;
+            self.last_number = clicked_value;
+            self.array_input[self.index] = self.last_item;
+            self.display(clicked_value);
+        };
 
         this.add_operator = function (clicked_value) {
-            if (inputs_array[index] === '') {
-                if (index === 0) {
-                    return;
-                }
-                inputs_array[index - 1] = operator;
-            }
-            index++;
-            inputs_array[index] = operator;
-            index++;
-            inputs_array[index] = '';
+
         };
 
-        this.add_number = function (clicked_value) {
-          /*  inputs_array[index] += clicked_value;*/
-            if (clicked_value === ".") {//decimal point is the first input
-                clicked_value = "0.";
-            }
-            inputs_array[index] = clicked_value;
-            index++;
-            this.display(clicked_value);
-        };
+
 
         this.delete_one = function () {
             inputs_array.pop();
@@ -75,8 +87,8 @@ $(document).ready(function() {
         };
 
         this.delete_all = function () {
-            this.inputs_array = [];
-            $('#output').text('');
+            inputs_array = [''];
+            $('output').text('');
         };
 
         /*this.equate = function () {
